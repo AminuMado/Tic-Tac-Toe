87fbdec71d7bbe4b7fdc1004ecc46ef5a245fadf
Aminu Mukhtar Mado <madoaminu@gmail.com>
2021-10-10 11:17:41 +0100

    add 3x3 gameboard dyanamically with data id  and added a hover effect

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("we in?")
+const mainContainer = document.querySelector(".main-container");
+function createDiv(){
+    for( let i = 0; i < 9; i++){
+        const createDiv = document.createElement("div");
+        mainContainer.appendChild(createDiv);
+        createDiv.setAttribute("data-Id", i);
+        createDiv.classList.add("grid-cell");    
+    };
+}
+createDiv();
